048d72121c0c8b93555814485fcd43eac1b59032
gulistan33 <gulistan33@gmail.com>
2021-09-29 20:27:11 -0700

    more changes

--- src/main/java/HelloMain.java
+++ src/main/java/HelloMain.java
@@ -2,5 +2,6 @@ public class HelloMain {
     public static void main(String[] args) {
         System.out.println("HelloMain");
         System.out.println("Developing feature");
+        System.out.println("Github repo is already published");
     }
 }
